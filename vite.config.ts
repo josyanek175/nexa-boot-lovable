31c29f0e29036d05086f98d9aba9116711947854
josyanek175 <josyanek175@gmail.com>
2026-04-23 05:49:04 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  server: {
-    host: true
-  },
-  preview: {
-    host: true,
-    allowedHosts: ['nexa.72.61.133.41.nip.io']
-  },
-  build: {
-    outDir: "dist"
+  vite: {
+    build: {
+      outDir: "dist",
+      ssr: false
+    }
   }
 });
