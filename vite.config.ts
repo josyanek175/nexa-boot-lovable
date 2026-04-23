20af143d35d6109620541c9abb5c0463f87886d1
josyanek175 <josyanek175@gmail.com>
2026-04-23 05:35:48 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,12 @@
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig();
+export default defineConfig({
+  server: {
+    host: true
+  },
+  preview: {
+    host: true,
+    allowedHosts: ['nexa.72.61.133.41.nip.io']
+  }
+});
